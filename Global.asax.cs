62bd15d0edc0d6b0eaa632759917f3d36ab8d933
Patrick Zenhäusern <pz@tenhouse.ch>
2015-07-16 08:22:26 +0200

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -12,3 +12,17 @@ namespace Nasicus.Toggl.Overtime
 		}
 	}
 }
+﻿using System.Web;
+using System.Web.Http;
+using Nasicus.Toggl.Overtime.App_Start;
+
+namespace Nasicus.Toggl.Overtime
+{
+	public class WebApiApplication : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+		}
+	}
+}
